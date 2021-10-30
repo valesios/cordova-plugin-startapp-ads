1ccdbcf5222f6b36dd6d4b0c4b4e0c67eb480c0b
Valeriy Skachko <45511899+valesios@users.noreply.github.com>
2021-10-31 01:26:37 +0300

    Update StartAppAdsPlugin.java

--- src/android/StartAppAdsPlugin.java
+++ src/android/StartAppAdsPlugin.java
@@ -23,7 +23,7 @@ import android.content.Intent;
 import android.os.Bundle;
 //import android.view.View;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.startapp.sdk.adsbase.Ad;
