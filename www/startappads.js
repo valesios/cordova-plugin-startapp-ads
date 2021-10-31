c133b473dcf9e86fd564a7590dca2b2db097a27e
Valeriy Skachko <45511899+valesios@users.noreply.github.com>
2021-10-31 11:04:04 +0200

    Update startappads.js

--- www/startappads.js
+++ www/startappads.js
@@ -2,13 +2,8 @@ var cordova = require('cordova');
 var exec = require('cordova/exec');
 
 var StartAppAds = {
-	init: function(appid, options) {
-		var DEFAULT_OPTIONS = { returnAd: true, splashAd: true };
-
-		if (typeof options !== "object") { options = DEFAULT_OPTIONS; }
-		options = Object.assign(DEFAULT_OPTIONS, options);
-
-		exec(function(){}, function(){}, "StartAppAdsPlugin", "initStartApp", [appid, !options.returnAd, !options.splashAd]);
+	init: function(appid) {
+		exec(function(){}, function(){}, "StartAppAdsPlugin", "initStartApp", [appid]);
 	},
 	showBanner: function() {
 		exec(function(){}, function(){}, "StartAppAdsPlugin", "showBanner", []);
@@ -18,10 +13,6 @@ var StartAppAds = {
 	},
 	showInterstitial: function() {
 		exec(function(){}, function(){}, "StartAppAdsPlugin", "showInterstitial", []);
-	},
-	loadRewardVideo: function(autoShow) {
-		autoShow = (typeof autoShow === "boolean") ? autoShow : true;
-		exec(function(){}, function(){}, "StartAppAdsPlugin", "loadRewardVideo", [ autoShow ]);
 	},
 	showRewardVideo: function() {
 		exec(function(){}, function(){}, "StartAppAdsPlugin", "showRewardVideo", []);
